3c4297d28d3947838ea5cac0961a657c141ee4aa
Connor <connoreaster@gmail.com>
2023-08-03 08:59:20 +0100

    Minor

--- SSO-ASP.NET.6-POC/SSO-ASP.NET.6-POC/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SSO-ASP.NET.6-POC/SSO-ASP.NET.6-POC/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -118,7 +118,7 @@ namespace SSO_ASP.NET._6_POC.Areas.Identity.Pages.Account
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
-                {
+                { 
                     return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
                 }
                 if (result.IsLockedOut)
